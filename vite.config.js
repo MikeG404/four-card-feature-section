a6f334929b80980d216682d7722863f522ef7365
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-08-27 08:34:58 +0200

    github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/four-card-feature-section"
+  base: '/four-card-feature-section/',
+    build: { outDir: 'docs' } // test
 })
